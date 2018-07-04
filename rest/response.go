5b6360085e9c00db8cf01b87c37d0102e1a0b975
Peter Mount <peter@retep.org>
2018-07-04 18:31:56 +0100

    Add streaming

--- rest/response.go
+++ rest/response.go
@@ -3,6 +3,8 @@ package rest
 import (
   "encoding/json"
   "encoding/xml"
+  "errors"
+  "io"
 )
 
 const (
@@ -12,50 +14,94 @@ const (
   TEXT_XML string = "text/xml"
 )
 
+var (
+  resposeUsed = errors.New( "Response already written to" )
+)
+
 // Send returns data to the client.
 // If the request has the Accept header of "text/xml" or "application/xml" then
 // the response will be in XML, otherwise in JSON.
-func (r *Rest) Send() {
-  if( !r.sent ) {
-
-    if r.status <= 0 {
-      r.status = 200
-    }
-
-    accept := r.GetHeader( "Accept" )
-    isXml := accept == TEXT_XML || accept == APPLICATION_XML
-    isJson := accept == TEXT_JSON || accept == APPLICATION_JSON
-
-    // Ensure we have a valid contentType default to APPLICATION_JSON if not
-    if !isXml && !isJson {
-      accept = APPLICATION_JSON
-      isJson = true
-    }
-
-    // Force the Content-Type
-    r.AddHeader( "Content-Type", accept )
-
-    // Until we get CORS handling correctly
-    r.AddHeader( "Access-Control-Allow-Origin", "*" )
-
-    // Write the headers
-    h := r.writer.Header()
-    for k, v := range r.headers {
-      h.Add( k, v)
-    }
-
-    // Write the status
-    r.writer.WriteHeader( r.status )
-
-    // Finally the content
-    if r.value != nil {
-      if isXml {
-        xml.NewEncoder( r.writer ).Encode( r.value )
-      } else {
-        json.NewEncoder( r.writer ).Encode( r.value )
-      }
-    }
-
-    r.sent = true
+func (r *Rest) Send() error {
+  if( r.sent ) {
+    return resposeUsed
   }
+  return r.send( nil )
+}
+
+// Write allows a functon to be called with a writer which can be used to
+// stream a response back to the client
+func (r *Rest) Write( f func( r *Rest, w io.Writer ) error ) (err error) {
+  if( r.sent ) {
+    return resposeUsed
+  }
+
+  rdr, wtr := io.Pipe()
+  defer wtr.Close()
+  defer rdr.Close()
+
+  // In a separate goroutine call the function to write the output
+  go func() {
+    err = f( r, wtr )
+    if err != nil {
+      wtr.CloseWithError( err )
+    }
+  }()
+
+  // Write the response. This will block against the function
+  err = r.send( rdr )
+
+  return err
+}
+
+// common send code
+func (r *Rest) send( reader io.Reader ) error {
+  r.sent = true
+
+  if r.status <= 0 {
+    r.status = 200
+  }
+
+  accept := r.GetHeader( "Accept" )
+  isXml := accept == TEXT_XML || accept == APPLICATION_XML
+  isJson := accept == TEXT_JSON || accept == APPLICATION_JSON
+
+  // Ensure we have a valid contentType default to APPLICATION_JSON if not
+  if !isXml && !isJson {
+    accept = APPLICATION_JSON
+    isJson = true
+  }
+
+  // Force the Content-Type if the response contentType is not set
+  if r.contentType == "" {
+    r.AddHeader( "Content-Type", accept )
+  }else {
+    r.AddHeader( "Content-Type", r.contentType )
+  }
+
+  // Until we get CORS handling correctly
+  r.AddHeader( "Access-Control-Allow-Origin", "*" )
+
+  // Write the headers
+  h := r.writer.Header()
+  for k, v := range r.headers {
+    h.Add( k, v)
+  }
+
+  // Write the status
+  r.writer.WriteHeader( r.status )
+
+  // Write from a reader
+  if reader != nil {
+    _, err := io.Copy( r.writer, reader )
+    return err
+  } else if r.value != nil {
+    // Finally the content, encode if an object
+    if isXml {
+      return xml.NewEncoder( r.writer ).Encode( r.value )
+    } else {
+      return json.NewEncoder( r.writer ).Encode( r.value )
+    }
+  }
+
+  return nil
 }
