6024f2b072a55c456994e2bc52975fe29d36a327
Peter Mount <peter@retep.org>
2018-08-17 20:24:08 +0100

    Add BeforeCommit/ExecBeforeCommit

--- kernel/db/tx.go
+++ kernel/db/tx.go
@@ -11,11 +11,13 @@ type Tx struct {
   tx    *sql.Tx
   // parent DBservice
   db    *DBService
+  // beforeCommit code
+  beforeCommit []TxHandler
   // ionCommit code
-  onCommit []onCommit
+  onCommit []OnCommit
 }
 
-type onCommit func() error
+type OnCommit func() error
 
 type TxHandler func( *Tx) error
 
@@ -43,6 +45,13 @@ func (s *DBService) Update( f TxHandler ) error {
 }
 
 func (tx *Tx) commit() error {
+  for _, f := range tx.beforeCommit {
+    err := f( tx )
+    if err != nil {
+      return err
+    }
+  }
+
   err := tx.tx.Commit()
   if err != nil {
     return err
@@ -91,7 +100,7 @@ func (tx *Tx) QueryRow(query string, args ...interface{}) *sql.Row {
 }
 
 // OnCommit will call the supplied function once this transaction commits
-func (tx *Tx) OnCommit( f func() error ) {
+func (tx *Tx) OnCommit( f OnCommit ) {
   tx.onCommit = append( tx.onCommit, f )
 }
 
@@ -111,5 +120,27 @@ func (tx *Tx) ExecOnCommit( query string, args ...interface{} ) {
 func (tx *Tx) UpdateOnCommit( f TxHandler ) {
   tx.OnCommit( func() error {
     return tx.db.Update( f )
+  })
+}
+
+// BeforeCommit will call the supplied function when the transaction is about to commit
+// but before the actual commit.
+func (tx *Tx) BeforeCommit( f TxHandler ) {
+  tx.beforeCommit = append( tx.beforeCommit, f )
+}
+
+// ExecBeforeCommit will execute a query once this transaction commits BUT BEFORE
+// the actual transaction commits.
+//
+// Unlike ExecOnCommit this runs within this transaction, it's just queued so that
+// it will be run before the transaction is finally Committed.
+// Note this will not be within a transaction, see UpdateOnCommit for that
+func (tx *Tx) ExecBeforeCommit( query string, args ...interface{} ) {
+  tx.BeforeCommit( func( tx *Tx ) error {
+    _, err := tx.db.db.Exec( query, args... )
+    if tx.db.Debug && err != nil {
+      log.Printf( "Error: %s\n%v", query, err )
+    }
+    return err
   })
 }
