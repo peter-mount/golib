f87636bd123498f19d154078420e8ec1150ad086
Peter Mount <peter@retep.org>
2018-09-12 15:20:43 +0100

    If no FileName provided then add -bucket-store command line argument or use BUCKETSTORE environment variable to get the bucket store file name

--- kernel/bolt/service.go
+++ kernel/bolt/service.go
@@ -4,22 +4,40 @@ package bolt
 
 import (
   bbolt "github.com/coreos/bbolt"
+  "github.com/peter-mount/golib/kernel"
+  "flag"
   "fmt"
+  "os"
   "time"
 )
 
 type BoltService struct {
   FileName  string
   db       *bbolt.DB
+  dbFile   *string
 }
 
 func (s *BoltService) Name() string {
   return "bolt:" + s.FileName
 }
 
-func (s *BoltService) PostInit() error {
+func (s *BoltService) Init( k *kernel.Kernel ) error {
   if s.FileName == "" {
-    return fmt.Errorf( "No filename supplied for bbolt" )
+    s.dbFile = flag.String( "bucket-store", "", "The file to store all buckets" )
+  }
+  return nil
+}
+
+func (s *BoltService) PostInit() error {
+  if s.FileName == "" && s.dbFile != nil {
+    s.FileName = *s.dbFile
+  }
+
+  if s.FileName == "" {
+    s.FileName = os.Getenv( "BUCKETSTORE" )
+  }
+  if s.FileName == "" {
+    return fmt.Errorf( "No store provided by -bucket-store or BUCKETSTORE" )
   }
   return nil
 }
